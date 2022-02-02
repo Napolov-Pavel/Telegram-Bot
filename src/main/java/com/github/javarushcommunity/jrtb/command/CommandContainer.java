3113e0518c7959c551bf4b4d45ea9bf21a21f0e6
Pavel <94539181+Napolov-Pavel@users.noreply.github.com>
2022-02-02 14:29:44 +0300

    Jrtb 1 step 5 (#20)
    * add BD
    
    * JRTB-1 added repository layer
    
    * fixed
    
    * fixed

--- src/main/java/com/github/javarushcommunity/jrtb/command/CommandContainer.java
+++ src/main/java/com/github/javarushcommunity/jrtb/command/CommandContainer.java
@@ -1,6 +1,7 @@
 package com.github.javarushcommunity.jrtb.command;
 
 import com.github.javarushcommunity.jrtb.service.SendBotMessageService;
+import com.github.javarushcommunity.jrtb.service.TelegramUserService;
 
 import java.util.HashMap;
 
@@ -10,12 +11,13 @@ public class CommandContainer {
     private final HashMap<String, Command> commandMap;
     private final Command unknownCommand;
 
-    public CommandContainer(SendBotMessageService sendBotMessageService) {
+    public CommandContainer(SendBotMessageService sendBotMessageService, TelegramUserService telegramUserService) {
         commandMap = new HashMap<String, Command>();
-        commandMap.put(START.getCommandName(), new StartCommand(sendBotMessageService));
-        commandMap.put(STOP.getCommandName(), new StopCommand(sendBotMessageService));
+        commandMap.put(START.getCommandName(), new StartCommand(sendBotMessageService, telegramUserService));
+        commandMap.put(STOP.getCommandName(), new StopCommand(sendBotMessageService, telegramUserService));
         commandMap.put(HELP.getCommandName(), new HelpCommand(sendBotMessageService));
         commandMap.put(NO.getCommandName(), new NoCommand(sendBotMessageService));
+        commandMap.put(STAT.getCommandName(), new StatCommand(sendBotMessageService, telegramUserService));
         unknownCommand = new UnknownCommand(sendBotMessageService);
     }
     public Command retrieveCommand(String commandIdentifier) {
